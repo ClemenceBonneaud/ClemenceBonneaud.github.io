121fd82fd7ce2f9808db35acaf554e79c20f2400
ClemenceBonneaud <clemence.bonneaud@gmail.com>
2022-07-13 18:24:40 +0200

    portfolio

---
+++ src/views/About.vue
@@ -0,0 +1,116 @@
+<!-- ////////////////////////////////////////////////// -->
+<!--                      TEMPLATE                      -->
+<!-- ////////////////////////////////////////////////// -->
+
+<template>
+    <!-- Header -->
+    <header>
+        <div class="header-border header-border--1"></div>
+        <div class="header-border header-border--2"></div>
+        <a href="#about"><h1>{{ title }}</h1></a>
+    </header>
+
+    <section class="section section__about" id="about">
+
+      <div class="about about__pro">
+        <figure>
+          <img alt="photo de Clémence Bonneaud AKA moi" src="/images/moi.jpg" /> 
+          <figcaption>Une tentative d'immitation de la Joconde</figcaption>
+        </figure>
+
+        <div class="about--texte">
+          <h2> Je suis une développeuse web junior avec une affinité pour le front-end.</h2>
+            <p> J'ai découvert la programmation à travers mon ancien travail dans la recherche scientifique
+              (R / MatLab) et j'ai décidé de tout arrêter pour me consacrer à cette discipline.</p>
+            <p>J'ai suivi la formation complète d'OpenClassrooms à travers laquelle j'ai travaillé sur de 
+              nombreux projets.</p>
+            <p>Je travaille actuellement avec JavaScript via Vue.js (bien qu'il m'arrive d'utiliser du JS natif), 
+              mais je ne suis pas contre apprendre un autre framework. J'ai déjà créé et travaillé avec des 
+              API RESTful, des bases de données (sql et no-sql) et sur les qualités SEO et accessibilité 
+              d'un site.
+              </p>
+        </div>
+      </div>
+
+      <div class="about about__perso">
+
+        <div class="about--texte">
+          <h2>Et en dehors du code ?</h2>
+            <p>Dans une vie antérieure j'étudiais les capacités cognitives des animaux, et plus précisément 
+              celle des chimpanzés. Qu'ils soient sauvages ou en parc animalier, mon but était de les observer, 
+              parfois de leur proposer des jeux libre d'accès, programmés sur écran tactile, pour voir de quoi 
+              ils sont capables.</p>
+            <p>Je suis de manière générale très curieuse, j'ai toujours envie d'apprendre de nouvelles choses.
+              C'est pourquoi à travers les années je me suis passionnée de voyages, de photo, de langues, de 
+              sports, de moto, de lectures, ...
+              </p>
+        </div>
+
+        <figure>
+          <img alt="photo de Clémence Bonneaud AKA moi" src="/images/moi2.jpg" /> 
+          <figcaption>Une tentative d'immitation de Jane Goodall</figcaption>
+        </figure>
+      </div>
+    </section>
+
+</template>
+
+<!-- ////////////////////////////////////////////////// -->
+<!--                       SCRIPT                       -->
+<!-- ////////////////////////////////////////////////// -->
+
+<script>
+
+export default {
+  name: "About",
+  path: "/about",
+  props: [""],
+  data() {
+    return {
+        hover: "",
+        title: "A propos",
+    };
+  }
+};
+</script>
+
+<!-- ////////////////////////////////////////////////// -->
+<!--                       STYLE                        -->
+<!-- ////////////////////////////////////////////////// -->
+
+<style lang="scss">
+
+.section__about {
+  margin-left: 50px;
+  width: 90%;
+  & figure {
+    width:  200px;
+    height: 200px;
+  }
+  & img {
+    width:  100%;
+    height: 100%;
+    border-radius: 80px;
+    object-fit: cover;
+  }
+  & figcaption {
+    font-size: 14px;
+    font-weight: 400;
+    text-align: center;
+    width: 90%;
+    margin-left:  auto;
+    margin-right: auto;
+    margin-top:   5px;
+  }
+}
+
+.about {
+  display: flex;
+  margin-bottom: 40px;
+  margin-top:    10px;
+  &--texte {
+    width: 70%;
+  }
+}
+
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,190 @@
+<!-- ////////////////////////////////////////////////// -->
+<!--                      TEMPLATE                      -->
+<!-- ////////////////////////////////////////////////// -->
+
+<template>
+    <!-- Header -->
+    <header>
+        <div class="header-border header-border--1"></div>
+        <div class="header-border header-border--2"></div>
+        <a href="#contacts"><h1>{{ title }}</h1></a>
+    </header>
+
+    <section class="section section__contact" id="contacts">
+
+      <article id="contact-recherche">
+        <h2>En ce moment, je suis à la recherche de :</h2>
+          <h3>Une opportunité en front-end en Suisse Romande (ou proche Suisse romande) ou en full-remote.</h3>
+            <p>Je cherche avant tout une entreprise dans laquelle je pourrais travailler en équipe 
+              avec d'autres développeurs, un endroit où je pourrais mettre à l'épreuve mes 
+              capacités, relever des challenges, et surtout continuer encore et toujours 
+              à apprendre et à progresser.
+            </p><br>
+      </article>
+
+      <article id="contact-liens">
+        <h2 id="titre-cartes">Alors si mon profil vous intéresse, ou si vous voulez simplement discuter :</h2>
+          <ul>
+            <li>
+              <span>Retrouvez-moi directement sur <br><br><a href="https://www.linkedin.com/in/cl%C3%A9mence-bonneaud-570a9514b/" target= "_blank">
+              LinkedIn</a><br><br> sous le nom "Clémence Bonneaud" (surprenant non ?)</span>
+              <div class="open open__top open__top-left"></div>
+              <div class="open open__top open--middle open__top-middle"></div>
+              <div class="open open__top open--right open__top-right"></div>
+              <div class="open open__bottom open--right open__bottom-right"></div>
+              <div class="open open__bottom open--middle open__bottom-middle"></div>
+              <div class="open open__bottom open__bottom-left"></div>
+            </li>
+
+            <li>
+              <span>Si vous préférez les email, vous pouvez m'en envoyer un à l'adresse 
+              <br><br><a href="mailto:clemence.bonneaud@gmail.com" target= "_blank">clemence.bonneaud@gmail.com</a><br><br> 
+              (toujours aussi imprévisible !)</span>
+              <div class="open open__top open__top-left"></div>
+              <div class="open open__top open--middle open__top-middle"></div>
+              <div class="open open__top open--right open__top-right"></div>
+              <div class="open open__bottom open--right open__bottom-right"></div>
+              <div class="open open__bottom open--middle open__bottom-middle"></div>
+              <div class="open open__bottom open__bottom-left"></div>
+            </li>
+
+            <li>
+              <span>Pour m'avoir directement, de vive voix, appelez moi au <br><br><a href="tel:+33683148024" target= "_blank">+33683148024</a><br><br> 
+            (celui-là normalement vous n'aviez pas pu le deviner)</span>
+              <div class="open open__top open__top-left"></div>
+              <div class="open open__top open--middle open__top-middle"></div>
+              <div class="open open__top open--right open__top-right"></div>
+              <div class="open open__bottom open--right open__bottom-right"></div>
+              <div class="open open__bottom open--middle open__bottom-middle"></div>
+              <div class="open open__bottom open__bottom-left"></div>
+            </li>
+          </ul>
+      </article>
+    </section>
+
+</template>
+
+<!-- ////////////////////////////////////////////////// -->
+<!--                       SCRIPT                       -->
+<!-- ////////////////////////////////////////////////// -->
+
+<script>
+
+export default {
+  name: "Contact",
+  path: "/contact",
+  props: [""],
+  data() {
+    return {
+        hover: "",
+        title: "Contactez-moi !",
+    };
+  }
+};
+</script>
+
+<!-- ////////////////////////////////////////////////// -->
+<!--                       STYLE                        -->
+<!-- ////////////////////////////////////////////////// -->
+
+<style lang="scss">
+
+.section__contact {
+  width: 90%;
+  margin-left:  auto;
+  margin-right: auto;
+
+  & li {
+    list-style-type: none;
+  }
+  & h2 {
+    margin-bottom: 50px;
+  }
+  & #titre-cartes {
+    margin-bottom: 100px;
+  }
+}
+
+#contact-recherche {
+  width: 100%;
+  text-align: center;
+  margin-bottom: 40px;
+}
+
+#contact-liens {
+  width: 100%;
+  text-align: center;
+  & ul {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0;
+    @media screen and (max-width: 1180px){
+      flex-direction: column;
+    }
+  }
+  & li {
+    margin-bottom: 10px;
+    height: 200px;
+    width: 300px;
+    display: flex;
+    align-items: center;
+    & span {
+      padding: 10px;
+      z-index: 1;
+    }
+    & a {
+      font-weight: 400;
+    }
+    &:hover {
+      & .open {
+        transform: rotate(180deg);
+      }
+    }
+    @media screen and (max-width: 1180px){
+      margin-left:  auto;
+      margin-right: auto;
+      margin-bottom: 100px;
+    }
+  }
+}
+
+.open {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  z-index: -1px;
+  transition: all 1000ms ease-in-out;
+  &__top {
+    margin-top: -100px;
+  }
+  &__bottom {
+    margin-top: 100px;
+  }
+  &--right {
+    margin-left: 200px;
+  }
+  &--middle {
+    margin-left: 100px;
+  }
+  &__top-left {
+    transform-origin: top left;
+  }
+  &__top-middle {
+    transform-origin: top;
+  }
+  &__top-right {
+    transform-origin: top right;
+  }
+  &__bottom-right {
+    transform-origin: bottom right;
+  }
+  &__bottom-middle {
+    transform-origin: bottom;
+  }
+  &__bottom-left {
+    transform-origin: bottom left;
+  }
+}
+
+</style>
